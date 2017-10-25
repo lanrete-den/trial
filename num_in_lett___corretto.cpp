0f79b36cf8feec63fbf3b5946f5e5b1f800ce44a
god asura <32990123+josura@users.noreply.github.com>
2017-10-25 15:34:37 +0200

    Update num_in_lett___corretto.cpp

--- num_in_lett___corretto.cpp
+++ num_in_lett___corretto.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<cstdlib>
+#include<string>
 #include<stdlib.h>
 #include<windows.h>
 
@@ -96,157 +97,248 @@ switch (x){
 							break;
 						case (19):
 							cout<<"diciannove";
-							break;	
+							break;
+						case (21):
+							cout<<"ventuno";
+							break;
+						case (28):
+							cout<<"ventotto";
+							break;
+						case (31):
+							cout<<"trentuno";
+							break;
+						case (38):
+							cout<<"trentotto";
+							break;
+						case (41):
+							cout<<"quarantuno";
+							break;
+						case (48):
+							cout<<"quarantotto";
+							break;
+						case (51):
+							cout<<"cinquantuno";
+							break;
+						case (58):
+							cout<<"cinquantotto";
+							break;
+						case (61):
+							cout<<"sessantuno";
+							break;
+						case (68):
+							cout<<"sessantotto";
+							break;
+						case (71):
+							cout<<"settantuno";
+							break;
+						case (78):
+							cout<<"settantotto";
+							break;
+						case (81):
+							cout<<"ottantuno";
+							break;
+						case (88):
+							cout<<"ottantotto";
+							break;
+						case (91):
+							cout<<"novantuno";
+							break;
+						case (98):
+							cout<<"novantotto";
+							break;		
+						default :
+							if (x<0){
+							cout<<"numero negativo, why";
+						} /*  else if (x==char && x==string){
+							cout<<"Perche' ?";
+						}     */
 						}
-if (x>=20 && x<30){
+	/*	if ((x%10)==1){
+			
+		} 
+		else if ((x%10)==8){
+		
+		} */
+if (x>=20 && x<30 && x!=21 && x!=28){
 	cout<<"venti";
-	Switch_ten(x-20);	
+	Switch_ten(x%10);	
 }
-else if (x>=30 && x<40){
+else if (x>=30 && x<40 && x!=31 && x!=38){
 	cout<<"trenta";
-	Switch_ten(x-30);
+	Switch_ten(x%10);
 }
-else if  (x>=40 && x<50){
+else if  (x>=40 && x<50 && x!=41 && x!=48){
 	cout<<"quaranta";
-	Switch_ten(x-40);
+	Switch_ten(x%10);
 }
-else if (x>=50 && x<60){
+else if (x>=50 && x<60 && x!=51 && x!=58){
 	cout<<"cinquanta";
-	Switch_ten(x-50);
+	Switch_ten(x%10);
 }	
-else if	(x>=60 && x<70){
+else if	(x>=60 && x<70 && x!=61 && x!=68){
 	cout<<"sessanta";
-	Switch_ten(x-60);			
+	Switch_ten(x%10);			
 }
-else if  (x>=70 && x<80){
+else if  (x>=70 && x<80 && x!=71 && x!=78){
 	cout<<"settanta";
-	Switch_ten(x-70);
+	Switch_ten(x%10);
 }
-else if (x>=80 && x<90){
+else if (x>=80 && x<90 && x!=81 && x!=88){
 	cout<<"ottanta";
-	Switch_ten(x-80);
+	Switch_ten(x%10);
 }	
-else if	(x>=90 && x<100){
+else if	(x>=90 && x<100 && x!=91 && x!=98){
 	cout<<"novanta";
-	Switch_ten(x-90);			
+	Switch_ten(x%10);			
 }
 }
 
  
 
 void Switch_thou(int x){
+		/*	switch(x){
+						case (108):
+							cout<<"centotto";
+							break;
+						case (208):
+							cout<<"duecentotto";
+							break;
+						case (308):
+							cout<<"trecentotto";
+							break;	
+						case (408):
+							cout<<"quattrocentotto";
+							break;
+						case (508):
+							cout<<"cinquecentotto";
+							break;
+						case (608):
+							cout<<"seicentotto";
+							break;
+						case (708):
+							cout<<"settecentotto";
+							break;		
+						case (808):
+							cout<<"ottocentotto";
+							break;	
+						case (908):
+							cout<<"novecentotto";
+							break;				
+}     */
+								
 if (x>=100 && x<200){
 	cout<<"cento";
-	Switch_hun(x-100);
+	Switch_hun(x%100);
 }
 else if (x>=200 && x<300){
 	cout<<"duecento";
-	Switch_hun(x-200);
+	Switch_hun(x%100);
 }
 else if (x>=300 && x<400){
 	cout<<"trecento";
-	Switch_hun(x-300);
+	Switch_hun(x%100);
 }
 else if  (x>=400 && x<500){
 	cout<<"quattrocento";
-	Switch_hun(x-400);
+	Switch_hun(x%100);
 }
 else if (x>=500 && x<600){
 	cout<<"cinquecento";
-	Switch_hun(x-500);
+	Switch_hun(x%100);
 }	
 else if	(x>=600 && x<700){
 	cout<<"seicento";
-	Switch_hun(x-600);
+	Switch_hun(x%100);
 }
 else if  (x>=700 && x<800){
 	cout<<"settecento";
-	Switch_hun(x-700);
+	Switch_hun(x%100);
 }
 else if (x>=800 && x<900){
 	cout<<"ottocento";
-	Switch_hun(x-800);
+	Switch_hun(x%100);
 }	
 else if	(x>=900 && x<1000){
 	cout<<"novecento";
-	Switch_hun(x-900);
+	Switch_hun(x%100);
 }
 
 
 	
 }
+
 void Switch_tenth(int x){
 if (x>=1000 && x<1100){
 	cout<<"mille";
-	Switch_hun(x-1000);
+	Switch_hun(x%1000);
 }	
 else if (x>=2000 && x<2100){
 	cout<<"duemila";
-	Switch_hun(x-2000);
+	Switch_hun(x%1000);
 }
 else if (x>=3000 && x<3100){
 	cout<<"tremila";
-	Switch_hun(x-3000);
+	Switch_hun(x%1000);
 }
 else if (x>=4000 && x<4100){
 	cout<<"quattromila";
-	Switch_hun(x-4000);
+	Switch_hun(x%1000);
 }
 else if (x>=5000 && x<5100){
 	cout<<"cinquemila";
-	Switch_hun(x-5000);
+	Switch_hun(x%1000);
 }
 else if (x>=6000 && x<6100){
 	cout<<"seimila";
-	Switch_hun(x-6000);
+	Switch_hun(x%1000);
 }
 else if (x>=7000 && x<7100){
 	cout<<"settemila";
-	Switch_hun(x-7000);
+	Switch_hun(x%1000);
 }
 else if (x>=8000 && x<8100){
 	cout<<"ottomila";
-	Switch_hun(x-8000);
+	Switch_hun(x%1000);
 }
 else if (x>=9000 && x<9100){
 	cout<<"duemila";
-	Switch_hun(x-9000);
+	Switch_hun(x%1000);
 }
 else if (x>=1100 && x<2000){
 	cout<<"mille";
-	Switch_thou(x-1000);
+	Switch_thou(x%1000);
 }
 else if (x>=2100 && x<3000){
 	cout<<"duemila";
-	Switch_thou(x-2000);
+	Switch_thou(x%1000);
 }
 else if (x>=3100 && x<4000){
 	cout<<"tremila";
-	Switch_thou(x-3000);
+	Switch_thou(x%1000);
 }
 else if  (x>=4100 && x<5000){
 	cout<<"quattromila";
-	Switch_thou(x-4000);
+	Switch_thou(x%1000);
 }
 else if (x>=5100 && x<6000){
 	cout<<"cinquemila";
-	Switch_thou(x-5000);
+	Switch_thou(x%1000);
 }	
 else if	(x>=6100 && x<7000){
 	cout<<"seimila";
-	Switch_thou(x-6000);
+	Switch_thou(x%1000);
 }
 else if  (x>=7100 && x<8000){
 	cout<<"settemila";
-	Switch_thou(x-7000);	
+	Switch_thou(x%1000);	
 }
 else if (x>=8100 && x<9000){
 	cout<<"ottomila";
-	Switch_thou(x-8000);
+	Switch_thou(x%1000);
 }	
 else if	(x>=9100 && x<10000){
 	cout<<"novemila";
-	Switch_thou(x-9000);
+	Switch_thou(x%1000);
 }	
 }
 
